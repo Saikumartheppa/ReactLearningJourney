18ce48a78ec95995da61d231a9201124988d5154
Saikumartheppa <saikumartheppa06@gmail.com>
2025-07-01 18:55:50 +0530

    Created Hello World using HTML & JS

---
+++ helloWorld/script.js
@@ -0,0 +1,5 @@
+const container = document.createElement("div");
+const heading = document.createElement("h1");
+heading.innerHTML= "Hello World Namasthe React";
+container.appendChild(heading);
+document.body.appendChild(container);
