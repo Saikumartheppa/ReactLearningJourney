b1cea1e0492ed556ea46a35d738756695fe65bd5
Saikumartheppa <saikumartheppa06@gmail.com>
2025-07-02 09:14:01 +0530

    Created Elements Using React(Nested Elements & Siblings

--- helloWorld/script.js
+++
@@ -1,5 +0,0 @@
-const container = document.createElement("div");
-const heading = document.createElement("h1");
-heading.innerHTML= "Hello World Namasthe React";
-container.appendChild(heading);
-document.body.appendChild(container);
