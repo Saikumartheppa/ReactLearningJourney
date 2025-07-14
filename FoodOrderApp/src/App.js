a0c256a3a76b04ce6b15371b337db62825e52d82
Saikumartheppa <saikumartheppa06@gmail.com>
2025-07-14 22:17:36 +0530

    Created children routes By using Outlet

--- FoodOrderApp/src/App.js
+++ FoodOrderApp/src/App.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import NavBar from "./components/NavBar";
@@ -6,33 +5,38 @@ import Body from "./components/Body";
 import About from "./components/About";
 import ErrorPage from "./components/ErrorPage";
 import Contact from "./components/Contact";
-import {createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import { createBrowserRouter, RouterProvider , Outlet } from "react-router-dom";
 
 const AppLayOut = () => {
   console.log("App is Running");
-  return ( 
+  return (
     <div className="appContainer">
       <NavBar />
-      <Body />
+      <Outlet />
     </div>
   );
 };
 
 const appRouter = createBrowserRouter([
   {
-    element:<AppLayOut/>,
-    path:"/",
-    errorElement:<ErrorPage />
+    element: <AppLayOut />,
+    path: "/",
+    children: [
+      {
+        element: <Body />,
+        path: "/",
+      },
+      {
+        element: <About />,
+        path: "/about",
+      },
+      {
+        element: <Contact />,
+        path: "/contact",
+      }
+    ],
+    errorElement: <ErrorPage />,
   },
-  {
-    element:<About />,
-    path:"/about",
-  },
-  {
-    element:<Contact />,
-    path:"/contact",
-  }
-])
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={appRouter} />);
