4fa23eb49f59c0df3bd0df83b0ba1c1c4b2efc46
Saikumartheppa <saikumartheppa06@gmail.com>
2025-07-14 22:02:45 +0530

    Episode-07 Introduction to Routes and Created Sample Routes

--- FoodOrderApp/src/App.js
+++ FoodOrderApp/src/App.js
@@ -1,16 +1,38 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import NavBar from "./components/NavBar";
 import Body from "./components/Body";
+import About from "./components/About";
+import ErrorPage from "./components/ErrorPage";
+import Contact from "./components/Contact";
+import {createBrowserRouter, RouterProvider } from "react-router-dom";
+
 
 const AppLayOut = () => {
   console.log("App is Running");
-  return (
+  return ( 
     <div className="appContainer">
       <NavBar />
       <Body />
     </div>
   );
 };
+
+const appRouter = createBrowserRouter([
+  {
+    element:<AppLayOut/>,
+    path:"/",
+    errorElement:<ErrorPage />
+  },
+  {
+    element:<About />,
+    path:"/about",
+  },
+  {
+    element:<Contact />,
+    path:"/contact",
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayOut />);
+root.render(<RouterProvider router={appRouter} />);
---
+++ FoodOrderApp/src/components/Contact.js
@@ -0,0 +1,7 @@
+const Contact = () => {
+    return (
+        <div><h1>Contact us</h1>
+        <h3>For any queries reach out foodorder234.io</h3></div>
+    )
+}
+export default Contact;
