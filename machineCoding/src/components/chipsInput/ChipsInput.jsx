ca8be657bb04a390059cfd60c243b873ce1982b2
Saikumartheppa <saikumartheppa06@gmail.com>
2026-07-25 16:45:01 +0530

    follow up question 2 : Pressing back space twice deletes a last chip , single press focuses the last chip

--- machineCoding/src/components/chipsInput/ChipsInput.jsx
+++ machineCoding/src/components/chipsInput/ChipsInput.jsx
@@ -4,9 +4,12 @@ import styles from "./style.module.scss";
 const ChipsInput = () => {
   const [inputText, setInputText] = useState("");
   const [chips, setChips] = useState([]);
-
+  const [selectedChipIndex , setSelectedChipIndex] = useState(null);
   const handleInputChange = (e) => {
     setInputText(e.target.value);
+    if(selectedChipIndex !== null){
+        setSelectedChipIndex(null);
+    }
   };
 
   const handleKeyDown = (e) => {
@@ -17,12 +20,22 @@ const ChipsInput = () => {
       e.preventDefault();
       setChips((prev) => [...prev, trimmedValue]);
       setInputText("");
+      return;
     } 
     // Follow up Question 1 :  Delete last chip on backspace if input is empty
-    else if (e.key === "Backspace" && !inputText && chips.length > 0) {
-      setChips((prev) => prev.slice(0, -1));
+    // else if (e.key === "Backspace" && !inputText && chips.length > 0) {
+    //   setChips((prev) => prev.slice(0, -1));
+    // }
+    // Follow Up Question 2 : Pressing back space twice deletes a last chip , single press focuses the last chip
+     if(e.key === "Backspace" && !inputText && chips.length > 0) {
+        if(selectedChipIndex === null){
+            setSelectedChipIndex(chips.length - 1);
+        }else{
+            setChips((prev) => prev.filter((_, index) => index !== selectedChipIndex))
+            setSelectedChipIndex(null); 
+        }
     }
-  };
+   };
 
   const handleDeleteChip = (indexToDelete) => {
     setChips((prev) => prev.filter((_, index) => index !== indexToDelete));
@@ -31,7 +44,7 @@ const ChipsInput = () => {
   return (
     <div className={styles["chips-input"]}>
       {chips?.map((chip, index) => (
-        <div key={`${chip}-${index}`} className={styles["chips-input__chip"]}>
+        <div key={`${chip}-${index}`} className={ `${styles["chips-input__chip"]} ${selectedChipIndex === index? styles["chips-input__chip--selected"] : ""}`}>
           <span>{chip}</span>
           <button
             type="button"
