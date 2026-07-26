c132f13904f9861ab40a79d6b4674d8bbdba0d40
Saikumartheppa <saikumartheppa06@gmail.com>
2026-07-26 20:04:33 +0530

    Implemented separators and paste functionality follow up 3 & 4

--- machineCoding/src/components/chipsInput/ChipsInput.jsx
+++ machineCoding/src/components/chipsInput/ChipsInput.jsx
@@ -4,14 +4,36 @@ import styles from "./style.module.scss";
 const ChipsInput = () => {
   const [inputText, setInputText] = useState("");
   const [chips, setChips] = useState([]);
-  const [selectedChipIndex , setSelectedChipIndex] = useState(null);
+  const [selectedChipIndex, setSelectedChipIndex] = useState(null);
+  const separators = [",", " ", ";"];
+
   const deleteChip = (indexToDelete) => {
-     setChips((prev) => prev.filter((_, index) => index !== indexToDelete));
-  }
+    setChips((prev) => prev.filter((_, index) => index !== indexToDelete));
+  };
+  const addChips = (values) => {
+  setChips((prev) => {
+    const seen = new Set(prev);
+
+    const newChips = values.filter((value) => {
+      const chip = value.trim();
+
+      if (!chip || seen.has(chip)) {
+        return false;
+      }
+
+      seen.add(chip);
+      return true;
+    }).map(chip => chip.trim());
+
+    return [...prev, ...newChips];
+  });
+
+  setInputText("");
+};
   const handleInputChange = (e) => {
     setInputText(e.target.value);
     if(selectedChipIndex !== null){
-        setSelectedChipIndex(null);
+      setSelectedChipIndex(null);
     }
   };
 
@@ -19,35 +41,57 @@ const ChipsInput = () => {
     const trimmedValue = inputText.trim();
 
     // Add Chip
-    if (e.key === "Enter" && trimmedValue) {
+    if (e.key === "Enter") {
       e.preventDefault();
-      setChips((prev) => [...prev, trimmedValue]);
-      setInputText("");
+      if (trimmedValue) {
+        addChips([trimmedValue]);
+      }
       return;
-    } 
+    }
+    // follow up 3 : If user clicks on keys like "," , " " , ";". add the chip
+    if (separators.includes(e.key)) {
+      e.preventDefault();
+
+      if (trimmedValue) {
+        addChips([trimmedValue]);
+      }
+      return;
+    }
     // Follow up Question 1 :  Delete last chip on backspace if input is empty
     // else if (e.key === "Backspace" && !inputText && chips.length > 0) {
     //   setChips((prev) => prev.slice(0, -1));
     // }
     // Follow Up Question 2 : Pressing back space twice deletes a last chip , single press focuses the last chip
-     if(e.key === "Backspace" && !inputText && chips.length > 0) {
-        if(selectedChipIndex === null){
-            setSelectedChipIndex(chips.length - 1);
-        }else{
-            deleteChip(selectedChipIndex);
-            setSelectedChipIndex(null); 
-        }
+    if (e.key === "Backspace" && !inputText && chips.length > 0) {
+      if (selectedChipIndex === null) {
+        setSelectedChipIndex(chips.length - 1);
+      } else {
+        handleDeleteChip(selectedChipIndex);
+        setSelectedChipIndex(null);
+      }
     }
-   };
-
+  };
+  
   const handleDeleteChip = (indexToDelete) => {
     deleteChip(indexToDelete);
+  };
+  // follow up 4 : If user copy & paste's values from note pad or some where.
+  const handlePaste = (e) => {
+    e.preventDefault();
+    const pastedText = e.clipboardData.getData("text");
+
+    const values = pastedText.split(/[,\n;\n' ']/);
+
+    addChips(values);
   };
 
   return (
     <div className={styles["chips-input"]}>
       {chips?.map((chip, index) => (
-        <div key={`${chip}-${index}`} className={ `${styles["chips-input__chip"]} ${selectedChipIndex === index? styles["chips-input__chip--selected"] : ""}`}>
+        <div
+          key={`${chip}-${index}`}
+          className={`${styles["chips-input__chip"]} ${selectedChipIndex === index ? styles["chips-input__chip--selected"] : ""}`}
+        >
           <span>{chip}</span>
           <button
             type="button"
@@ -67,6 +111,7 @@ const ChipsInput = () => {
         value={inputText}
         onChange={handleInputChange}
         onKeyDown={handleKeyDown}
+        onPaste={handlePaste}
       />
     </div>
   );
