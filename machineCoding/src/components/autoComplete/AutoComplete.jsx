87dd3ac81124f2441beda79cb5238a394da97418
Saikumartheppa <saikumartheppa06@gmail.com>
2026-07-27 10:28:21 +0530

    Basic Functionality done

--- machineCoding/src/components/autoComplete/AutoComplete.jsx
+++ machineCoding/src/components/autoComplete/AutoComplete.jsx
@@ -1,4 +1,27 @@
+import { useState , useEffect } from "react";
+import styles from "./style.module.scss";
 const AutoComplete = () => {
-   return <h2>Auto Complete</h2>
+   const [searchText , setSeachText] = useState('');
+   const [suggestions , setSuggestions] = useState([]);
+   const fetchData = async ()=>{
+      const response = await fetch(`https://dummyjson.com/recipes/search?q=${searchText}`);
+      const data = await response.json();
+      setSuggestions(data?.recipes);
+      console.log(data.recipes);
+   }
+   useEffect(() => {
+     fetchData();
+   }, [searchText])
+   return (
+    <div className={styles["auto-complete"]}>
+        <h2>Auto Complete</h2>
+        <input className={styles["auto-complete__input-field"]} type="text" placeholder="Search..." onChange={(e) => setSeachText(e.target.value)}></input>
+        <div className={styles["auto-complete__suggestions-container"]}>
+            {suggestions.map((suggestion) => {
+               return <p key={suggestion.id} className={styles["auto-complete__suggestion"]}>{suggestion.name}</p>
+            })}
+        </div>
+    </div>
+   )
 }
 export default AutoComplete;
