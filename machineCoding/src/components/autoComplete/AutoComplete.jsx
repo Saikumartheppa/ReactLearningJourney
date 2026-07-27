45a3249c85cb97d67165d6da204059f0bce4d6d7
Saikumartheppa <saikumartheppa06@gmail.com>
2026-07-27 18:24:53 +0530

    Optimized code and Added functionality to select a suggestion

--- machineCoding/src/components/autoComplete/AutoComplete.jsx
+++ machineCoding/src/components/autoComplete/AutoComplete.jsx
@@ -1,23 +1,60 @@
 import { useState, useEffect } from "react";
 import styles from "./style.module.scss";
+const SuggestionsList = ({ loading, suggestions, onSelect}) => {
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!suggestions.length) {
+    return <p>No Results Available.</p>;
+  }
+
+  return suggestions.map((suggestion) => (
+    <p
+      key={suggestion.id}
+      className={styles["auto-complete__suggestion"]}
+      onMouseDown={()=> 
+        onSelect(suggestion)
+    }
+    >
+      {suggestion.name}
+    </p>
+  ));
+}
 const AutoComplete = () => {
   const [searchText, setSearchText] = useState("");
   const [suggestions, setSuggestions] = useState([]);
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [cacheSuggestions, setCacheSuggestions] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSelect = (suggestion) => {
+     setSearchText(suggestion.name);
+     setShowSuggestions(false);
+  }
   const fetchData = async () => {
-    if(!searchText.trim()) return;
-    if (cacheSuggestions[searchText]) {
-      setSuggestions(cacheSuggestions[searchText]);
+    const trimmedSearchValue = searchText.trim();
+    if (!trimmedSearchValue) {
+      setSuggestions([]);
+      return;
+    }
+    if (cacheSuggestions[trimmedSearchValue]) {
+      setSuggestions(cacheSuggestions[trimmedSearchValue]);
     } else {
-      const response = await fetch(
-        `https://dummyjson.com/recipes/search?q=${searchText}`,
-      );
-      const data = await response.json();
-      setSuggestions(data?.recipes);
-      setCacheSuggestions((prev) => {
-        return { ...prev, [searchText]: data?.recipes };
-      });
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          `https://dummyjson.com/recipes/search?q=${trimmedSearchValue}`,
+        );
+        const data = await response.json();
+        setSuggestions(data?.recipes);
+        setCacheSuggestions((prev) => {
+          return { ...prev, [trimmedSearchValue]: data?.recipes };
+        });
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
     }
   };
   useEffect(() => {
@@ -38,29 +75,16 @@ const AutoComplete = () => {
         placeholder="Search..."
         onChange={(e) => setSearchText(e.target.value)}
         onFocus={() => setShowSuggestions(true)}
-        onBlur={() => {
-          setShowSuggestions(false);
-          setSearchText("");
-        }}
+        onBlur={() =>  {
+            setShowSuggestions(false);
+            setSearchText("");
+            setSuggestions([]);
+        }
+        }
       ></input>
       {showSuggestions && (
-        <div className={styles["auto-complete__suggestions-container"]}>
-          {suggestions.length ? (
-            suggestions.map((suggestion) => {
-              return (
-                <p
-                  key={suggestion.id}
-                  className={styles["auto-complete__suggestion"]}
-                >
-                  {suggestion.name}
-                </p>
-              );
-            })
-          ) : (
-            <span>
-              No Results Available.Please try with different keyword!!!!
-            </span>
-          )}
+        <div className={styles["auto-complete__suggestions-container"]} onMouseDown={(e) => e.preventDefault()}>
+         <SuggestionsList loading={isLoading} suggestions={suggestions} onSelect={handleSelect}/>
         </div>
       )}
     </div>
