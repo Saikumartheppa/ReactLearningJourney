9a7e4743b58d0907b98f91d5a95933ef387acd5a
Saikumartheppa <saikumartheppa06@gmail.com>
2026-08-13 21:26:18 +0530

    Implemented Auto focus while entering and clearing the input fields

--- machineCoding/src/components/otp-input/OTPInput.jsx
+++ machineCoding/src/components/otp-input/OTPInput.jsx
@@ -1,13 +1,43 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./style.module.scss";
-import {OTP_INPUT_DIGITS as otpInputDigits} from "../constants"
-const OTPInput = ()=> {
-    const [inputArr , setInputArr] = useState(new Array(otpInputDigits).fill(1));
-    return <div className={styles["otp-container"]}>
-        <h2 className={styles["otp-container__heading"]}>Validate OTP</h2>
-        {inputArr.map((input , index) => {
-            return <input className={styles["otp-container__input"]} key={index} type='number' aria-controls="none"/>
-        })}
+import { OTP_INPUT_DIGITS as otpInputDigits } from "../constants";
+const OTPInput = () => {
+  const [inputArr, setInputArr] = useState(new Array(otpInputDigits).fill(""));
+  const refArr = useRef([]);
+  const handleOnChange = (value, index) => {
+    if (isNaN(value)) {
+      return;
+    }
+    const newArr = [...inputArr];
+    newArr[index] = value.slice(-1);
+    setInputArr(newArr);
+    value.trim() && refArr.current[index + 1]?.focus();
+  };
+  const handleOnKeyDown = (e , index) => {
+    if(!e.target.value && e.key === 'Backspace'){
+        refArr.current[index - 1]?.focus();
+    }
+  }
+  useEffect(() => {
+    refArr.current[0]?.focus();
+  }, []);
+  return (
+    <div className={styles["otp-container"]}>
+      <h2 className={styles["otp-container__heading"]}>Validate OTP</h2>
+      {inputArr.map((input, index) => {
+        return (
+          <input
+            className={styles["otp-container__input"]}
+            key={index}
+            type="text"
+            value={input}
+            ref={(input) => (refArr.current[index] = input)}
+            onChange={(e) => handleOnChange(e.target.value, index)}
+            onKeyDown={(e) => handleOnKeyDown(e , index)}
+          />
+        );
+      })}
     </div>
-}
+  );
+};
 export default OTPInput;
