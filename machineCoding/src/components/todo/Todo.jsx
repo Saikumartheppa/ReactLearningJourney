fa9d17794e0f621765a43daaf3b0dfaafbddac10
Saikumartheppa <saikumartheppa06@gmail.com>
2026-08-29 19:09:40 +0530

    feature : Implemented Edit functionality. Now users can edit the todo title by doubleClicking on the title or clicking the edit button and fixed input field bug

--- machineCoding/src/components/todo/Todo.jsx
+++ machineCoding/src/components/todo/Todo.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
-import {TodoInput, TodoList} from "../todo";
+import { TodoInput, TodoList } from "../todo";
 import styles from "./style.module.scss";
 const Todo = () => {
   const [todoInputField, setTodoInputField] = useState("");
   const [todoList, setTodoList] = useState([]);
+  const [editingTodoId, setEditingTodoId] = useState(null);
   const handleInputField = (value) => {
-    const trimmedValue = value.trim();
-    setTodoInputField(trimmedValue);
+    setTodoInputField(value);
   };
   const handleAddTodoItem = (value) => {
     const trimmedValue = value.trim();
-    if(!trimmedValue){
+    if (!trimmedValue) {
       return;
     }
     const todoItem = {
@@ -19,20 +19,38 @@ const Todo = () => {
       isCompleted: false,
     };
     setTodoList((prev) => [...prev, todoItem]);
-    setTodoInputField('');
+    setTodoInputField("");
   };
   const handleCheckboxClick = (toBeCompletedTodoId) => {
-     const updatedList = todoList.map((todo) => {
-        if(todo.id === toBeCompletedTodoId){
-           todo.isCompleted = !todo.isCompleted
-        }
-        return todo;
-     })
-     setTodoList(updatedList);
-  }
+    const updatedList = todoList.map((todo) => {
+      if (todo.id === toBeCompletedTodoId) {
+        todo.isCompleted = !todo.isCompleted;
+      }
+      return todo;
+    });
+    setTodoList(updatedList);
+  };
   const handleDeleteTodo = (toBeDeletedTodoId) => {
-    const updatedTodoList = todoList.filter((todo) => todo.id !== toBeDeletedTodoId);
+    const updatedTodoList = todoList.filter(
+      (todo) => todo.id !== toBeDeletedTodoId,
+    );
     setTodoList(updatedTodoList);
+  };
+  const handleEditTodo = (toBeEditedTodoId) => {
+    setEditingTodoId(toBeEditedTodoId);
+  };
+  const handleCancelTodo = () => {
+    setEditingTodoId(null);
+  };
+  const handleSaveTodo = (toBeSavedTodoId , EditedValue) => {
+    const trimmedValue = EditedValue.trim();
+    if(!trimmedValue){
+      return;
+    }
+    const updatedTodoList = todoList.map((todo) => 
+      todo.id === toBeSavedTodoId ? {...todo , title : trimmedValue} : todo)
+    setTodoList(updatedTodoList);
+    setEditingTodoId(null);
   }
   return (
     <div className={styles["todo"]}>
@@ -42,7 +60,15 @@ const Todo = () => {
         handleInputField={handleInputField}
         handleAddTodoItem={handleAddTodoItem}
       />
-      <TodoList todoList={todoList} handleCheckboxClick={handleCheckboxClick} handleDeleteTodo={handleDeleteTodo}/>
+      <TodoList
+        todoList={todoList}
+        handleCheckboxClick={handleCheckboxClick}
+        handleDeleteTodo={handleDeleteTodo}
+        editingTodoId={editingTodoId}
+        handleEditTodo={handleEditTodo}
+        handleCancelTodo={handleCancelTodo}
+        handleSaveTodo={handleSaveTodo}
+      />
     </div>
   );
 };
--- machineCoding/src/components/todo/TodoList.jsx
+++ machineCoding/src/components/todo/TodoList.jsx
@@ -1,21 +1,110 @@
+import { useState } from "react";
 import styles from "./style.module.scss";
-const TodoItem = ({todoItem , handleCheckboxClick , handleDeleteTodo}) => {
-  const {id , title , isCompleted} = todoItem;
+const TodoItem = ({
+  todoItem,
+  handleCheckboxClick,
+  handleDeleteTodo,
+  editingTodoId,
+  handleEditTodo,
+  handleCancelTodo,
+  handleSaveTodo
+}) => {
+  const { id, title, isCompleted } = todoItem;
+  const [editedValue, setEditedValue] = useState(title);
+  const isEditing = (id) => {
+    return editingTodoId === id;
+  };
+  const handleEditedValue = (value) => {
+    setEditedValue(value);
+  };
   return (
-    <div className={styles['todo__todoItem']}>
-      <input type="checkBox" checked={isCompleted} className={styles['todo__checkBox']} onChange={() => handleCheckboxClick(id)}/>
-      <p className={`${isCompleted ?  styles['todo--title-completed'] : ""}`}>{title}</p>
-      <button className={styles['todo--deleteBtn']} onClick={() => handleDeleteTodo(id)}>Delete</button>
+    <div className={styles["todo__todoItem"]}>
+      {!isEditing(id) && (
+        <input
+          type="checkBox"
+          checked={isCompleted}
+          className={styles["todo__checkBox"]}
+          onChange={() => handleCheckboxClick(id)}
+        />
+      )}
+      {isEditing(id) ? (
+        <input
+          className={styles["todo__input-field"]}
+          value={editedValue}
+          onChange={(e) => handleEditedValue(e.target.value)}
+          onKeyDown={(e) => e.key === 'Enter' && handleSaveTodo(id , e.target.value)}
+        />
+      ) : (
+        <p className={`${isCompleted ? styles["todo--title-completed"] : ""}`} onDoubleClick={(e)=> handleEditTodo(id)}>
+          {title}
+        </p>
+      )}
+      {isEditing(id) ? (
+        <button
+          className={styles["todo__editBtn"]}
+          onClick={() => {
+            handleSaveTodo(id, editedValue)
+            setEditedValue(editedValue);
+          }}
+        >
+          Save
+        </button>
+      ) : (
+        <button
+          className={styles["todo__editBtn"]}
+          onClick={() => handleEditTodo(id)}
+        >
+          Edit
+        </button>
+      )}
+
+      {isEditing(id) ? (
+        <button
+          className={styles["todo--deleteBtn"]}
+          onClick={() => {
+            handleCancelTodo(id);
+            setEditedValue(title);
+          }}
+        >
+          Cancel
+        </button>
+      ) : (
+        <button
+          className={styles["todo--deleteBtn"]}
+          onClick={() => handleDeleteTodo(id)}
+        >
+          Delete
+        </button>
+      )}
     </div>
   );
 };
 const TodoList = (props) => {
-  const {todoList , handleCheckboxClick , handleDeleteTodo} = props;
+  const {
+    todoList,
+    handleCheckboxClick,
+    handleDeleteTodo,
+    editingTodoId,
+    handleEditTodo,
+    handleCancelTodo,
+   handleSaveTodo
+  } = props;
   return (
-    <div className={styles['todo__todoList']}>
-        {todoList.map((todoItem) => {
-            return <TodoItem key={todoItem.id} todoItem ={todoItem} handleCheckboxClick={handleCheckboxClick} handleDeleteTodo={handleDeleteTodo}/>
-        })}
+    <div className={styles["todo__todoList"]}>
+      {todoList?.map((todoItem) => {
+        return (
+          <TodoItem
+            key={todoItem?.id}
+            todoItem={todoItem}
+            handleCheckboxClick={handleCheckboxClick}
+            handleDeleteTodo={handleDeleteTodo}
+            editingTodoId={editingTodoId}
+            handleEditTodo={handleEditTodo}
+            handleCancelTodo={handleCancelTodo}
+            handleSaveTodo={handleSaveTodo}
+          />
+        );
+      })}
     </div>
   );
 };
